532d980015081d43b20682b4fe225ba08a013e33
Sagar Samrat <64777977+Sagar17032001@users.noreply.github.com>
2025-12-03 11:43:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1292,7 +1292,7 @@ async function runAll(email) {
     
       setStatus("Results saved!");
     } catch (e) {
-      setStatus("Completed All Test!");
+      setStatus("Completed All Tests!");
       console.error(e);
     }
     
